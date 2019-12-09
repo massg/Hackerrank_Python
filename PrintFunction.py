3103ccdc9613a6efa0d62a2dc25216c681fa04f2
Sreesruthi P K <31540953+massg@users.noreply.github.com>
2019-12-09 22:57:29 +0530

    Create PrintFunction.py

---
+++ PrintFunction.py
@@ -0,0 +1,7 @@
+#Print Function
+#Link: hackerrank.com/challenges/python-print/copy-from/54339225
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1) :
+        print (i,end='')
